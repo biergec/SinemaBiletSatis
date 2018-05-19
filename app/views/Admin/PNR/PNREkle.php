b4f640bd7b6a92ef903eecee7b7eb93f3fe77b2c
Yunus Yığci <37595924+yunusyigci@users.noreply.github.com>
2018-05-19 04:21:13 +0300

    b

---
+++ app/views/Admin/PNR/PNREkle.php
@@ -0,0 +1,31 @@
+<?php require VDIR.'/Admin/header.php' ?>
+
+<form action="./?url=PNR/PNREklemePost" method="Post">
+  <div class="form-group">
+      
+      
+  <label for="labelPNR">PNR İndirim Miktarı(Yüzde)</label>
+    <input type="text" class="form-control"name="indirimMiktari">
+  <br>
+    <label for="labelKod">PNR Kodu</label>
+    <input type="text" class="form-control"name="pnrKod">
+    <br>
+    <label for="labelStok">Stok Sayısı</label>
+    <input type="text" class="form-control"name="stok">
+  </div>
+  <button type="Submit" value="Kaydet" class="btn btn-primary">PNR Ekle</button>
+  
+  <?php 
+  if($uyari){
+    echo $uyari;
+  }
+
+?>
+</form> 
+<hr>
+<?php 
+ if($result){
+  echo $result;
+}
+?>
+<?php require VDIR.'/Admin/footer.php' ?>
